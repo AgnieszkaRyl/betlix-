8f935a5465584b353ec52793297e2c5ddf2cfdf0
AgnieszkaRyl <ryl.agnieszka123@gmail.com>
2022-06-09 23:54:37 +0200

    add library for carousel

--- src/pages/Movies/Movies.tsx
+++ src/pages/Movies/Movies.tsx
@@ -8,6 +8,11 @@ import styles from "./Movies.module.scss";
 import {useWindowSize} from "../../hooks/useWindowSize";
 import arrowRight from "./../../assets/arrowRight.svg"
 import arrowLeft from "./../../assets/arrowLeft.svg"
+import classNames from "classnames";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import {Navigation} from "swiper";
 
 export interface Image {
     Id: number;
@@ -54,21 +59,7 @@ const MoviesList = () => {
     const [movies, setMovies] = useState<Entity[]>();
     const {logged} = useContext(VideoContext);
 
-    const containerRef = useRef<any>(null);
-    const sliderRef = useRef<any>(null);
-    const elementRef = useRef<any>(null);
 
-    const rootRef = useRef<HTMLDivElement>(null);
-
-    const {width} = useWindowSize();
-    const [containerWidth, setContainerWidth] = useState(0);
-    const [cardItemsSliderWidth, setCardItemsSliderWidth] = useState(0);
-    const [cardItemsNumber, setCardItemsNumber] = useState(0);
-    const [cardItemWidth, setCardItemWidth] = useState(0);
-    const [sliderIndex, setSliderIndex] = useState(0);
-    const [carouselOffset, setCarouselOffset] = useState(0);
-
-    console.log("width", cardItemWidth)
     console.log("logged", logged)
     useEffect(() => {
         fetch("https://thebetter.bsgroup.eu/Media/GetMediaList", {
@@ -89,35 +80,6 @@ const MoviesList = () => {
             .catch(err => console.error(err))
     }, [])
 
-    useEffect(() => {
-        if (movies){
-            const cardItemsContainerWidth = containerRef.current.offsetWidth;
-            const cardItemsSliderWidth = sliderRef.current.offsetWidth;
-            const cardItemsNumber = sliderRef.current.childNodes.length;
-            const cardItemWidth = elementRef.current.offsetWidth;
-
-            setContainerWidth(cardItemsContainerWidth);
-            setCardItemsSliderWidth(cardItemsSliderWidth);
-            setCardItemsNumber(cardItemsNumber);
-            setCardItemWidth(cardItemWidth);
-        }
-    }, [width]);
-
-    useEffect(() => {
-        const carouselOffset = cardItemWidth * sliderIndex - cardItemsSliderWidth + containerWidth;
-        console.log("carouselOffset", carouselOffset);
-        setCarouselOffset(carouselOffset);
-    }, [cardItemWidth, sliderIndex, cardItemsSliderWidth, containerWidth, width]);
-
-    const handleSlideLeft = () => {
-        if (sliderIndex === 0) return;
-        setSliderIndex(sliderIndex - 1);
-    };
-
-    const handleSlideRight = () => {
-        if (sliderIndex === cardItemsNumber) return;
-        setSliderIndex(sliderIndex + 1);
-    };
     const getImageTypedFrame = (images: Image[]): string => {
         const imageFrame = images.find(image => image.ImageTypeCode === "FRAME")
         if (imageFrame === undefined) {
@@ -126,7 +88,7 @@ const MoviesList = () => {
         return imageFrame.Url;
     }
     if (!movies) {
-        return <Spinner />
+        return <Spinner/>
     }
     // console.log(movies)
     // console.log("logged", logged)
@@ -134,37 +96,23 @@ const MoviesList = () => {
 
     return (
         <div className="container">Tutaj bedzie lista filmow
-            <div className={styles.wrapper}>
-                <div className={styles.innerTrack} ref={containerRef}>
-                    <div className={styles.track} ref={sliderRef}
-                         style={{
-                             transform: `translateX(-${cardItemWidth * sliderIndex}px)`,
-                         }}>
-                        {movies.map((el) =>
-                            <Link to={"/player/" + el.Id} key={el.Id} >
-                                <Card variant="outlined" className={styles.movieContainer} ref={elementRef}>
-                                    {el.Title}
-                                    <img src={getImageTypedFrame(el.Images)} style={{width: "120px", height: "80px"}}/>
-                                </Card>
-                            </Link>
-                        )}
-                    </div>
-                    <div className="house-teaser-card__nav">
-                        {sliderIndex !== 0 && (
-                            <button className={styles.button} onClick={() => handleSlideLeft()}>
-                                <img src={arrowLeft}/>
-                            </button>
-                        )}
-
-                        {sliderIndex !== cardItemsNumber && (
-                            <button className={styles.button} onClick={() => handleSlideRight()}>
-                                <img src={arrowRight}/>
-                            </button>
-                        )}
-                    </div>
-                </div>
-            </div>
-
+            <Swiper
+                spaceBetween={10}
+                slidesPerView={4}
+                navigation
+                modules={[Navigation]}
+            >
+                {movies.map((el) =>
+                    <SwiperSlide>
+                        <Link to={"/player/" + el.Id} key={el.Id} className={classNames(styles.linkJeden, styles.link)}>
+                            <Card variant="outlined" className={styles.movieContainer}>
+                                {el.Title}
+                                <img src={getImageTypedFrame(el.Images)} style={{width: "120px", height: "80px"}}/>
+                            </Card>
+                        </Link>
+                    </SwiperSlide>
+                )}
+            </Swiper>
         </div>
     );
 }
